d881365d4f0c8dbdb8d0dbe3406bd047fff028b6
Quaqu Hubert <kwakuhubert@gmail.com>
2019-04-24 14:33:26 +0000

    First commit

---
+++ app/src/main/java/com/icp/wastemanagementsystem/MainActivity.java
@@ -0,0 +1,24 @@
+package com.icp.wastemanagementsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
